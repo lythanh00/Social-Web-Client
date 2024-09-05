44546b890915fc95db03f198f7bfc52f0bda7767
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2024-09-05 10:44:22 +0700

    feat: logout

--- src/auth/chat/index.tsx
+++ src/auth/chat/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Layout } from 'antd';
+import Logout from '../../components/Logout';
 
 const { Header, Content, Sider } = Layout;
 
 const Chat: React.FC = () => {
-  return <div>abc</div>;
+  return (
+    <div>
+      abc <Logout />
+    </div>
+  );
 };
 
 export default Chat;
